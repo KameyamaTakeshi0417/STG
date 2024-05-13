092578c7a92e0efb18d8df657571a646e5d00b0b
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2024-05-14 00:31:36 +0900

    プレイヤーが弾打てるようになった

--- My project/Assets/scripts/Player.cs
+++ My project/Assets/scripts/Player.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     public float HP;
     public float Pow;
-    public Vector2 moveSpeed;
+    public float moveSpeed;
     public GameObject bullet;
     public float bulletSpeed;
     public float BulletSpan;
+    public Vector3 watch;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       // bullet = Instantiate(bullet, transform.position, Quaternion.identity);
+
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 入力の角度を取得する
+        float moveAngle = Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * Mathf.Rad2Deg;
+        watch = new Vector2(Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad), Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.Deg2Rad));
+        // 入力がない場合は何もしない
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            rb.velocity = Vector2.zero;
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GameObject bulletPrefab = Instantiate(bullet, transform.position, Quaternion.identity);
+            bulletPrefab.GetComponent<Bullet_Base>().setRotate(watch);
+            Destroy(bulletPrefab, 3);
+        }
+            // キャラクターを移動させる
+            rb.velocity = Quaternion.AngleAxis(moveAngle, Vector3.forward) * Vector2.right * moveSpeed;
+
+        // キャラクターの向きを変える
+        transform.rotation = Quaternion.AngleAxis(moveAngle, Vector3.forward);
     }
 }
