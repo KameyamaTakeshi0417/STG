468c264e1c86ebfe070e00cf94400919052b6b2f
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2024-05-19 21:00:11 +0900

    playerの攻撃ボタンをspace->マウス左クリックに変更。

--- My project/Assets/scripts/Player.cs
+++ My project/Assets/scripts/Player.cs
@@ -11,12 +11,14 @@ public class Player : MonoBehaviour
     public float moveSpeed;
     public GameObject bullet;
     public float bulletSpeed;
-    public float BulletSpan;
+    public float BulletSpan;//フレーム。
+    public bool onCoolTime;
     public Vector3 watch;
 
     // Start is called before the first frame update
     void Start()
     {
+        onCoolTime=false;
        // bullet = Instantiate(bullet, transform.position, Quaternion.identity);
 
         rb = GetComponent<Rigidbody2D>();
@@ -29,13 +31,15 @@ public class Player : MonoBehaviour
         float moveAngle = Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * Mathf.Rad2Deg;
         watch = new Vector2(Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad), Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.Deg2Rad));
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButton(0)&&onCoolTime==false)
         {
+onCoolTime=true;
             float ratio = 1.5f;
             Vector3 createPos = transform.position + new Vector3(watch.x * ratio, watch.y * ratio, watch.z);
             GameObject bulletPrefab = Instantiate(bullet, createPos, Quaternion.identity);
             bulletPrefab.GetComponent<Bullet_Base>().setRotate(watch);
             bulletPrefab.GetComponent<Bullet_Base>().setBulletSpeed(bulletSpeed);
+            StartCoroutine("coolTime");
             //Destroy(bulletPrefab, 3);
         }
         // 入力がない場合は何もしない
@@ -50,5 +54,18 @@ public class Player : MonoBehaviour
 
         // キャラクターの向きを変える
         transform.rotation = Quaternion.AngleAxis(moveAngle, Vector3.forward);
+    }
+    private IEnumerator coolTime(){
+onCoolTime=true;
+int count=0;
+while(true){
+if(count>=BulletSpan){
+onCoolTime=false;
+    yield break;
+}
+count++;
+    yield return new WaitForEndOfFrame();
+}
+        yield return null;
     }
 }
