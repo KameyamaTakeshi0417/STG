ce1bd0a2b6ee97e03d028f4a6ff3139334a240f8
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2024-05-20 23:11:47 +0900

    カメラがプレイヤーの散歩後ろに行くようになった。酔う。

--- My project/Assets/scripts/CameraControl.cs
+++ My project/Assets/scripts/CameraControl.cs
@@ -1,24 +1,28 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    public Vector3 cameraPosition;
+    public Vector3 cameraPositionOffset;
     public GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+public float range=5.0f;
     void Update()
     {
         ObeyPlayer();
     }
-    void ObeyPlayer(){
-transform.position=cameraPosition+player.transform.position;
 
+    void ObeyPlayer()
+    {
+        // プレイヤーの回転角度を取得
+        float rotationZ = player.transform.eulerAngles.z;
+
+        // 角度をラジアンに変換
+        float radians = rotationZ * Mathf.Deg2Rad;
+
+        // 向きベクトルを計算
+        Vector3 direction = new Vector3(Mathf.Cos(radians)*range, Mathf.Sin(radians)*range, -10f);
+
+        // カメラの新しい位置を計算
+        transform.position = player.transform.position + cameraPositionOffset + direction;
     }
 }
