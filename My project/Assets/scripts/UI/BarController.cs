5f3f094168fedf2523e8a38017a785c8a1b14aa4
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2024-05-26 09:44:06 +0900

    UI大苦戦。 その他いろいろ。

---
+++ My project/Assets/scripts/UI/BarController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarController : MonoBehaviour
+{
+    public int min = 0;
+    public int max = 100;
+    public int currentValue = 0;
+
+    public Image barImage;
+    public Text valueText;
+
+    public Button increaseButton;
+    public Button decreaseButton;
+
+    private int maxBarUnits = 100; // バーの目盛りの最大単位数
+
+    void Start()
+    {
+        // ボタンのクリックイベントにリスナーを追加
+        increaseButton.onClick.AddListener(IncreaseValue);
+        decreaseButton.onClick.AddListener(DecreaseValue);
+
+        // 初期値の設定
+        UpdateBar();
+    }
+
+    void IncreaseValue()
+    {
+        if (currentValue < max)
+        {
+            currentValue += 10; // 10単位で増加
+            UpdateBar();
+        }
+    }
+
+    void DecreaseValue()
+    {
+        if (currentValue > min)
+        {
+            currentValue -= 10; // 10単位で減少
+            UpdateBar();
+        }
+    }
+
+    void UpdateBar()
+    {
+        // バーの長さを更新
+        float fillAmount = (float)currentValue / maxBarUnits;
+        barImage.fillAmount = fillAmount;
+
+        // テキストを更新
+        valueText.text = currentValue.ToString();
+    }
+}
