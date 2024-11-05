487e81ca8ccca0d6b070053f447ddad13dbdaa2f
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2024-11-06 01:31:07 +0900

    休憩でフェードを実装

---
+++ My project/Assets/scripts/ingameSystem/restRoom.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restRoom : MonoBehaviour
+{
+    public bool canRest;
+    public bool useRest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        useRest = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canRest && !useRest)
+        {
+            GameObject.Find("fadeBoard").GetComponent<FadeBoard>().callFadeScreen();
+            useRest=true;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        canRest = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        canRest = false;
+    }
+}
