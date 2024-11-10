5cafcfc53f1c1ef96895fb4b7c86193479505a55
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2024-11-11 00:51:38 +0900

    明日は雷管(=発射時効果)の実装となる。

--- My project/Assets/scripts/ingameSystem/Bullet/薬莢(Case)/ExplosionCase.cs
+++ My project/Assets/scripts/ingameSystem/Bullet/薬莢(Case)/ExplosionCase.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 public class ExplosionCase : Case_Base
 {
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     protected override IEnumerator move()
     {
 
--- My project/Assets/scripts/ingameSystem/Bullet/雷管(Primer)/ExplosionPrimer.cs
+++ My project/Assets/scripts/ingameSystem/Bullet/雷管(Primer)/ExplosionPrimer.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExplosionPrimer : MonoBehaviour
+public class ExplosionPrimer : Primer_Base
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    protected override IEnumerator Fire()
     {
-        
+        //弾丸を生成する
+        GameObject bulletPrefab = Instantiate(Resources.Load<GameObject>("Objects/Effect_Explosion"), gameObject.transform.position, Quaternion.identity);
+        bulletPrefab.GetComponent<Effect_Explosion>().startExplosion(30, 50);
+
+        //発射時の効果をここに記載する
+
+        yield break;
     }
 }
