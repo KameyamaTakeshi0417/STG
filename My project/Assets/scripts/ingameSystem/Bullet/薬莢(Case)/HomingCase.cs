86ba8039adf24f92dcc3381f3dcdd5e7855b9695
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2024-11-28 23:06:28 +0900

    テクスチャいろいろ追加。当たり判定も変更。

--- My project/Assets/scripts/ingameSystem/Bullet/薬莢(Case)/HomingCase.cs
+++ My project/Assets/scripts/ingameSystem/Bullet/薬莢(Case)/HomingCase.cs
@@ -5,24 +5,20 @@ using UnityEngine;
 public class HomingCase : Case_Base
 {
     GameObject targetEnemy;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
+    // Start is called before the first frame update
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() { }
 
-    }
     protected override IEnumerator move()
     {
-
         int count = 0;
         Rigidbody2D rb;
         //弾の発射
         rb = gameObject.GetComponent<Rigidbody2D>();
+        GameObject player = GameObject.Find("Player");
         targetEnemy = GameObject.Find("Player").GetComponent<Player>().getTargetEnemy();
         Vector3 targetWay = new Vector3(0, 0, 0);
         Vector2 force = new Vector2(rotate.x, rotate.y) * Speed;
@@ -31,6 +27,13 @@ public class HomingCase : Case_Base
             targetWay = targetEnemy.transform.position - transform.position;
             Vector3.Normalize(targetWay);
             force = new Vector2(targetWay.x, targetWay.y) * (Speed * 0.001f);
+        }
+        else
+        {
+            targetWay = player.GetComponent<Player>().watch;
+            Vector3.Normalize(targetWay);
+            force = new Vector2(targetWay.x, targetWay.y) * (Speed * 0.001f);
+            DestroyTime=100f;
         }
         rb.velocity = force;
 
@@ -47,11 +50,16 @@ public class HomingCase : Case_Base
                 force = new Vector2(targetWay.x, targetWay.y) * (Speed * 0.001f);
                 rb.velocity = force;
             }
+            else
+            {
+                force = new Vector2(targetWay.x, targetWay.y) * (Speed * 0.01f);
+                rb.velocity = force;
+            }
             count++;
-            yield return new WaitForSeconds(0.01f);
+            yield return new WaitForEndOfFrame();
         }
 
-        Destroy(myBullet);
+        Destroy(this.gameObject);
         yield break;
     }
 }
--- My project/Assets/scripts/ingameSystem/Bullet/雷管(Primer)/HomingPrimer.cs
+++ My project/Assets/scripts/ingameSystem/Bullet/雷管(Primer)/HomingPrimer.cs
@@ -5,14 +5,58 @@ using UnityEngine;
 public class HomingBullet : Primer_Base
 {
     // Start is called before the first frame update
-    void Start()
+    public override void StrikePrimer()
     {
-        
+        EquipManager equipManager = FindGameManager("GameManager").GetComponent<EquipManager>();
+        // 現在装備している弾丸、ケース、プライマーを取得
+        GameObject activeBullet = equipManager.GetActiveBullet();
+        GameObject activeCase = equipManager.GetActiveCase();
+        GameObject bulletPrefab;
+        float ratio = 1.5f;
+        Vector3 watch = FindGameManager("Player").GetComponent<Player>().watch;
+        float bulletSpeed = FindGameManager("Player").GetComponent<Player>().bulletSpeed;
+        Vector3 createPos = transform.position + (watch * ratio);
+
+        bulletPrefab = Instantiate(
+            Resources.Load<GameObject>("Objects/Bullet/NormalBullet"),
+            createPos,
+            Quaternion.identity
+        );
+
+        // 弾丸の基本ステータスを設定
+        Bullet_Base bulletScript = bulletPrefab.GetComponent<Bullet_Base>();
+
+        bulletScript.setStatus(watch, bulletSpeed, 10);
+
+        Case_Base caseScript;
+        System.Type caseType;
+
+        //何もついてないとき、通常薬莢装備
+        caseScript = Resources
+            .Load<GameObject>("Objects/Case/HomingCase")
+            .GetComponent<Case_Base>();
+        caseType = caseScript.GetType();
+        bulletPrefab.AddComponent(caseType);
+
+        // 弾丸の発射
+        bulletScript?.fire();
     }
 
-    // Update is called once per frame
-    void Update()
+    GameObject FindGameManager(string targetName)
     {
-        
+        // すべての GameObject を取得する
+        GameObject[] allGameObjects = FindObjectsOfType<GameObject>();
+
+        // すべてのオブジェクトをループして "GameManager" の名前を持ち、DontDestroyOnLoad にあるか確認
+        foreach (GameObject obj in allGameObjects)
+        {
+            if (obj.name == targetName && obj.scene.name == null)
+            {
+                return obj; // GameManager が見つかったら返す
+            }
+        }
+
+        // 見つからなかった場合は null を返す
+        return null;
     }
 }
