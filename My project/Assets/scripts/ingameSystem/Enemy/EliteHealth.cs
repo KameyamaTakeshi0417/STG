e4a6c007ffef656857b5bd3535ae1704f9e159b2
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2024-12-12 03:17:27 +0900

    もうちょいで実装できそう

--- My project/Assets/scripts/ingameSystem/Enemy/EliteHealth.cs
+++ My project/Assets/scripts/ingameSystem/Enemy/EliteHealth.cs
@@ -5,15 +5,20 @@ using UnityEngine.UI;
 
 public class EliteHealth : Health
 {
-    CircleGauge[] circleGauge = new CircleGauge[3];
+    public CircleGauge[] circleGauge = new CircleGauge[3];
     public int HealthPhase;
     public int LifeCount = 1;
     public GameObject[] CircleHPBar = new GameObject[3];
+    public GameObject canvasInstance;
 
     void Start()
     {
         currentHP = HP;
         m_handler = gameObject.GetComponent<HPBar_Base>();
+        for (int i = 0; i < 3; i++)
+        {
+            circleGauge[i] = CircleHPBar[i].GetComponent<CircleGauge>();
+        }
     }
 
     void Awake() { }
@@ -22,14 +27,14 @@ public class EliteHealth : Health
     public override void TakeDamage(float damage)
     {
         currentHP -= damage;
-        circleGauge[LifeCount].setMaxHP(HP);
-        circleGauge[LifeCount].setCurrentHP(currentHP);
+        circleGauge[LifeCount - 1].setMaxHP(HP);
+        circleGauge[LifeCount - 1].setCurrentHP(currentHP);
         if (hpSlider != null)
         {
             EliteSliderUpdate();
         }
         // Debug.Log(gameObject.name + " took " + damage + " damage. Remaining HP: " + currentHP);
-        if (currentHP <= 0)
+        if (LifeCount <= 0 && currentHP <= 0)
         {
             if (hpSlider != null)
             {
@@ -45,17 +50,12 @@ public class EliteHealth : Health
         hpSlider.value = currentHP; //スライダは０〜1.0で表現するため最大HPで割って少数点数字に変換
         if (hpSlider.value <= 0)
         {
-            LifeCount -= 1;
             currentHP = HP;
-        }
-        if (LifeCount <= 0 && currentHP <= 0)
-        {
-            if (hpSlider != null)
-            {
-                Destroy(hpSlider);
-            }
+            hpSlider.value = currentHP;
 
-            Die();
+            circleGauge[LifeCount].setMaxHP(HP);
+            circleGauge[LifeCount].setCurrentHP(currentHP);
+            LifeCount -= 1;
         }
     }
 
@@ -99,31 +99,29 @@ public class EliteHealth : Health
 
     public override void setSlideHPBar()
     {
-        GameObject canvasInstance = Instantiate(
-            Resources.Load<GameObject>("UI/EliteEnemyHPCanvas"),
-            gameObject.transform.position,
-            Quaternion.identity
-        );
         canvasInstance.GetComponent<HPBarFollower>().setTargetTransform(gameObject.transform);
         canvasInstance.transform.localPosition = new Vector3(0, 2, 0); // 必要に応じてオフセットを調整
 
         hpSlider = canvasInstance.transform.Find("HPBar").GetComponent<Slider>();
         if (hpSlider != null)
         {
+            SetCircleBar(LifeCount);
             // HPバーの初期設定
             hpSlider.maxValue = HP;
             hpSlider.value = (float)currentHP;
 
             // 円形ゲージの設定
-            circleGauge[LifeCount] = canvasInstance.GetComponentInChildren<CircleGauge>();
-            if (circleGauge != null)
+            for (int i = 0; i < LifeCount; i++)
             {
-                // HPバーの初期設定
-                hpSlider.maxValue = HP;
-                hpSlider.value = (float)currentHP; // HPバーの最初の値を現在のHPに設定
-                circleGauge[LifeCount].setMaxHP(HP);
-                circleGauge[LifeCount].setCurrentHP(currentHP);
-                SetCircleBar(LifeCount);
+                circleGauge[i] = canvasInstance.GetComponentInChildren<CircleGauge>();
+                if (circleGauge != null)
+                {
+                    // HPバーの初期設定
+                    hpSlider.maxValue = HP;
+                    hpSlider.value = (float)currentHP; // HPバーの最初の値を現在のHPに設定
+                    circleGauge[i].setMaxHP(HP);
+                    circleGauge[i].setCurrentHP(currentHP);
+                }
             }
         }
         else
