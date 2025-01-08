30ab80ad09f7b79dde06e42d7919bcb231bd2084
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2025-01-09 00:38:51 +0900

    TFT楽しみ過ぎてぜんぜんやれてねえ・・・が、肌感覚的に明日には実装完了しそうな感じがする

---
+++ My project/Assets/scripts/outGameSystem/Marchant/MarchantManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarchantManager : MonoBehaviour
+{
+    public GameObject targetCanvas;
+    public GameObject AmmoMergeButton;
+    public GameObject[] AmmoGoods = new GameObject[5];
+
+    public GameObject[] RelicGoods = new GameObject[3];
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+}
