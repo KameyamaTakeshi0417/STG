13b3f8593d657085df5382918d98d805aaebb30c
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2025-01-12 02:51:50 +0900

    とりあえずイベントハンドラー作った

---
+++ My project/Assets/scripts/outGameSystem/MIsteryEvent/EventHandler/_EventHandlerBase.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _EventHandlerBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject[] ButtonUI;
+
+    public virtual void Action1() { }
+
+    public virtual void Action2() { }
+
+    public virtual void Action3() { }
+
+    public virtual void Action4() { }
+}
