ed86cd450f34e45fa222d6cd9289b619a5b3db69
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2025-03-01 06:15:35 +0900

    ペタルバレットの挙動を少し追加。

---
+++ My project/Assets/scripts/ingameSystem/Enemy/Elite/PetalBullet_Himawari.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetalBullet_Himawari : PetalBullet_Base
+{
+    void Awake()
+    {
+        cycleCount = 0;
+    }
+}
