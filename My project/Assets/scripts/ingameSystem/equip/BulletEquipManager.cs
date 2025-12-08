e4bec2536390d39cfbc464b3096574ac498c27c7
Takeshi_Kameyama <neverforgetmatsushima@gmail.com>
2025-12-08 18:44:27 +0900

    追加：antiGravity

---
+++ My project/Assets/scripts/ingameSystem/equip/BulletEquipManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEquipManager : MonoBehaviour
+{
+    Bullet_Base[] equipBullet=new Bullet_Base[3];
+    Case_Base[] equipCase=new Case_Base[3];
+    Primer_Base[] equipPrimer=new Primer_Base[3];
+    // Start is called before the first frame update
+    void Start()
+    {
+        //セーブデータ読み込み
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //記入必要なし？
+    }
+}
